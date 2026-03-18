be305fd9b1e8bfe2d583f35a0635d0c3458842bb
Carlos S <sanchezvargascarlos1@gmail.com>
2026-03-17 20:22:09 -0600

    header del get options

--- api/reportes.php
+++ api/reportes.php
@@ -1,8 +1,15 @@
 <?php
 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 
 require '../config/database.php';
 
